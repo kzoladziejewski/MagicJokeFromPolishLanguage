572252b08e0f25f270d48034dbe721fd92ce15af
Kacper <kzoladziejewski@interia.pl>
2020-08-19 19:10:04 +0200

    finish project xD

--- app/app.py
+++ app/app.py
@@ -1,6 +1,72 @@
+import requests
+
 class MagicJokeFromPolishLanguage:
+
+    def __init__(self):
+        self.base_url = "https://polski-slownik.pl/"
+        self.base_page = "https://polski-slownik.pl/wszystkie-slowa-jezyka-polskiego.php"
+        self.all_words = []
+        self.jokes = []
 
     def compare_string(self, basic_string, cutet_string):
         if basic_string[1:] == cutet_string:
             return True
         return False
+
+    def get_all_hrefs_from_page(self):
+        returned = requests.get(self.base_page).text
+        returned = returned.split("\n")
+        list_of_sub_url = []
+        for element in returned:
+                sub_url = self.__cut_url(element)
+                list_of_sub_url.append(sub_url)
+        return list_of_sub_url
+
+    def __cut_url(self, element):
+        if "\" href=" in element and "favicon.ico" not in element and "title" not in element:
+            sub_url = element[element.find("href=\""):element.find("class")]
+            sub_url = sub_url.replace("href=", "").replace("\"", "").replace(" ", "")
+            return sub_url
+
+    def get_all_subhrefs_from_page(self, list_of_suburl):
+        list_of_sub_url = []
+        for url in list_of_suburl:
+            returned= requests.get("{}{}".format(self.base_url, url)).text
+            returned = returned.split("<td>")
+
+            for element in returned:
+                sub_url = self.__cut_url(element)
+                if sub_url:
+                    list_of_sub_url.append(sub_url)
+        return list_of_sub_url
+
+    def get_words(self, url_to_call):
+        returned = requests.get("{}{}".format(self.base_url, url_to_call)).text
+        returned = returned.split("itemprop=\"itemListElement\"")
+
+        for element in returned:
+            if element.startswith(">") and "</span" in element:
+                word = element[:element.find("</span")][1:]
+                self.all_words.append(word)
+
+    def generate_jokes(self):
+        for first_word in self.all_words:
+            for second_word in self.all_words:
+                if self.compare_string(first_word, second_word):
+                    self.jokes.append(self.__generate_joke(first_word, second_word))
+
+    def __generate_joke(self, first_word, second_word):
+        return "Jak jest {} bez {} ? \n {} \n\n!".format(first_word, second_word, first_word[0])
+
+    def save_jokes(self):
+        with open("joke.txt") as file:
+            file.write(self.jokes)
+
+if __name__ == "__main__":
+    mjfpl = MagicJokeFromPolishLanguage()
+    url_list = mjfpl.get_all_hrefs_from_page()
+    url_sub_list = mjfpl.get_all_subhrefs_from_page(url_list)
+    for element in url_sub_list:
+        mjfpl.get_words()
+    mjfpl.generate_jokes()
+    mjfpl.save_jokes()
--- tests/test_app.py
+++ tests/test_app.py
@@ -5,6 +5,9 @@ from app.app import MagicJokeFromPolishLanguage
 class TestApp:
     mjfpl = MagicJokeFromPolishLanguage()
 
+    alfabet = ["a", "ą", "b", "c", "ć", "d", "e", "ę", "f", "g", "h", "i", "j", "k", "l", "ł", "m", "n", "ń", "o", "ó",
+               "p", "q", "r", "s",
+               "ś", "t", "u", "v", "w", "x", "y", "z", "ź", "ż"]
     @pytest.mark.parametrize("basic_string, cutet_string",[("mucha","ucha"), ("kupa", "upa")])
     def test_compare_string_positive(self,basic_string, cutet_string):
         assert self.mjfpl.compare_string(basic_string, cutet_string) == True
@@ -12,3 +15,30 @@ class TestApp:
     @pytest.mark.parametrize("basic_string, cutet_string",[("muchy","ucha"), ("alalala", "lalalal")])
     def test_compare_string_negative(self, basic_string, cutet_string):
         assert self.mjfpl.compare_string(basic_string, cutet_string) == False
+
+    def test_get_all_hrefs_from_page(self):
+        returned_tested = self.mjfpl.get_all_hrefs_from_page()
+        assert len(returned_tested) == 35
+        guard = 0
+        for _ in self.alfabet:
+            assert "wszystkie-slowa-jezyka-polskiego.php?id=na-litere-{}".format(_) == returned_tested[guard]
+            guard+=1
+
+    def test_get_all_subhrefs_from_page(self):
+        list_subhrefs = []
+        for _ in self.alfabet:
+            list_subhrefs.append("wszystkie-slowa-jezyka-polskiego.php?id=na-litere-{}".format(_))
+
+        returned = self.mjfpl.get_all_subhrefs_from_page(list_subhrefs)
+        assert len(returned) == 370
+
+    def test_get_words(self):
+        url = "wszystkie-slowa-jezyka-polskiego.php?id=6-literowe-na-litere-a"
+        self.mjfpl.get_words(url)
+        assert "abbami" in self.mjfpl.all_words
+        assert "abacie" in self.mjfpl.all_words
+        assert "ażurze" in self.mjfpl.all_words
+
+    def test_generator_joke(self):
+        self.mjfpl.all_words = ["abacie", "ażurze", "abbami", "bacie", "żurze", "bbami"]
+        self.mjfpl.generate_jokes()
