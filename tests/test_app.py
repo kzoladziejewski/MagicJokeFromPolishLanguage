b17f15ff692878b67da4d997b4404426810d7781
Kacper <kzoladziejewski@interia.pl>
2020-08-30 16:00:38 +0200

    next tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -17,11 +17,11 @@ class TestApp(object):
 
     @pytest.mark.parametrize("basic_string, cutet_string",[("mucha","ucha"), ("kupa", "upa")])
     def test_compare_string_positive(self,basic_string, cutet_string, mjfpl):
-        assert mjfpl.compare_string(basic_string, cutet_string) == True
+        assert mjfpl.compare_string(basic_string, cutet_string)
 
     @pytest.mark.parametrize("basic_string, cutet_string",[("muchy","ucha"), ("alalala", "lalalal")])
     def test_compare_string_negative(self, basic_string, cutet_string, mjfpl):
-        assert mjfpl.compare_string(basic_string, cutet_string) == False
+        assert not mjfpl.compare_string(basic_string, cutet_string)
 
     def test_get_all_hrefs_from_page(self, mjfpl):
         returned_tested = mjfpl.get_all_hrefs_from_page()
@@ -68,7 +68,7 @@ class TestApp(object):
         for litera in self.alfabet:
             mjfpl.all_words.append("a{}a".format(litera))
         mjfpl.all_words.extend(lista)
-        assert mjfpl.merge_find(looking = "afa", lista_words = mjfpl.all_words) == True
+        assert mjfpl.merge_find(looking = "afa", lista_words = mjfpl.all_words)
 
     @pytest.mark.parametrize("lista, look",([
         (['ccc'], 'ccc'),
@@ -79,13 +79,14 @@ class TestApp(object):
         (['aaa','ccc','kkk','mmm','zzz'], 'ccc'),
         (['aaa','ccc','kkk','lll','mmm','zzz'], 'kkk'),
         (['aaa','ccc','kkk','lll','mmm','zzz'], 'ccc'),
+        (['aaa','ccc','kkk','lll','mmm','zzz'], 'zzz'),
         (['aaa', 'bbb', 'ccc', 'ddd', 'eee', 'fff', 'ggg', 'hhh', 'iii', 'jjj', 'kkk', 'lll', 'mmm', 'nnn', 'ooo',
           'ppp', 'qqq', 'rrr', 'sss', 'ttt', 'uuu', 'vvv', 'www', 'xxx', 'yyy', 'zzz'], 'ccc'),
         (['aaa', 'bbb', 'ccc', 'ddd', 'eee', 'fff', 'ggg', 'hhh', 'iii', 'jjj', 'kkk', 'lll', 'mmm', 'nnn', 'ooo',
           'ppp', 'qqq', 'rrr', 'sss', 'ttt', 'uuu', 'vvv', 'www', 'xxx', 'yyy', 'zzz'], 'xxx'),
     ]))
     def test_merge_find_lista_words_exactly_in_center(self, mjfpl, lista, look):
-        assert mjfpl.merge_find(look, lista_words=lista) == True
+        assert mjfpl.merge_find(look, lista_words=lista)
 
     @pytest.mark.parametrize("lista",([],["baa"]))
     def test_merge_find_negative(self, lista, mjfpl):
@@ -93,5 +94,5 @@ class TestApp(object):
         for litera in self.alfabet:
             mjfpl.all_words.append("a{}a".format(litera))
             elo.append("{}{}{}".format(litera,litera,litera))
-        assert mjfpl.merge_find(looking = "bbb", lista_words=mjfpl.all_words) == False
+        assert not mjfpl.merge_find(looking = "bbb", lista_words=mjfpl.all_words)
 
