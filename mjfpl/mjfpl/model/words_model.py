c418e740fe2a2373ca9d61385ec164100b82e7a7
Kacper <kzoladziejewski@interia.pl>
2022-03-21 00:53:17 +0100

    changes

--- mjfpl/mjfpl/model/words_model.py
+++ mjfpl/mjfpl/model/words_model.py
@@ -1,4 +1,4 @@
-from db import db
+from mjfpl.mjfpl.db import db
 
 class WordsModel(db.Model):
     __tablename__ = "words"
--- mjfpl/mjfpl/scripts/migrate_database.py
+++ mjfpl/mjfpl/scripts/migrate_database.py
@@ -3,6 +3,22 @@ Script for migrate data from old database to new database
 """
 
 import sys
+import pathlib
 
-if len(sys.argv) == 2:
-    print(sys.argv)
+from mjfpl.mjfpl.model.words_model import WordsModel
+
+if not len(sys.argv) == 3:
+
+    sys.exit()
+
+old_database = pathlib.Path.cwd().parent.joinpath(sys.argv[1])
+new_database = pathlib.Path.cwd().parent.joinpath(sys.argv[2])
+
+if not old_database.is_file():
+    print("Not exist old database")
+    sys.exit()
+if not new_database.is_file():
+    print("Not exist new database")
+    sys.exit()
+
+wm = WordsModel
